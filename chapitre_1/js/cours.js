112e97a787750191a1bba687dad68924f5d6f7d6
celine GA <celstuv@yahoo.fr>
2020-10-12 14:36:59 +0200

    premier cours

--- chapitre_1/js/cours.js
+++ chapitre_1/js/cours.js
@@ -9,3 +9,12 @@ if (document.body.nodeType === document.ELEMENT_NODE) { //un nœud "élément" (
 
 //Accès au 1er enfant du noeud body
 console.log(document.body.childNodes[1]);
+
+// Affiche les noeuds enfants
+for (var i = 0; i < document.body.childNodes.length; i++) {
+  console.log(document.body.childNodes[i]);
+}
+
+var h1 = document.body.childNodes[1];
+console.log(h1.parentNode); // Affiche le noeud Body
+console.log(document.parentNode); //Affiche null : document n'a aucun noeud parent
---
+++ chapitre_2/js/cours.js
@@ -0,0 +1,8 @@
+console.log(document.body.childNodes[5].childNodes[1]);
+
+
+var titresElt = document.getElementsByTagName("h2");
+console.log(titresElt[0]); //Merveilles du monde antique
+console.log(titresElt[1]); //Nouvelles merveilles du monde
+console.log(titresElt[2]); //Références
+console.log(titresElt.length);
